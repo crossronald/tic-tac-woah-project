bcac6cbf331c519066b44ed11bdf75377a86b00d
Ronald Cross <ronaldjamescross@gmail.com>
2020-09-08 17:23:03 -0400

    final commit hopefully!

--- curl-scripts/gameCurl/create-game 2.sh
+++
@@ -1,7 +0,0 @@
-curl "https://tic-tac-toe-api-production.herokuapp.com/games" \
-  --include \
-  --request POST \
-  --header "Content-Type: application/json" \
-  --header "Authorization: Token token=b9696a0a8239614085c435aabfe2c66c"
-
-echo
